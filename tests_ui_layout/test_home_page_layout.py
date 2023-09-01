f31933ab82dadfb4e061a34701f30aaca8dc8af9
mgerin <marin_gerin@abv.bg>
2023-09-01 16:54:34 +0300

    Project structure

--- tests_ui_layout/test_home_page_layout.py
+++ tests_ui_layout/test_home_page_layout.py
@@ -1,5 +1,5 @@
 from playwright.sync_api import Playwright, sync_playwright, expect
-from home_page_elements import HomePage
+from pom.home_page_elements import HomePage
 
 def about_us_section_verbiage(playwright: Playwright):
     browser = playwright.chromium.launch(headless=False)
